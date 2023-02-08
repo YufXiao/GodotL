efe5c242eab32f83be0cff5826a4386802c8a296
yufeng <yufeng.xiao@uzh.ch>
2023-02-08 04:08:13 +0100

    add new scripts

--- Mob.gd
+++ Mob.gd
@@ -17,3 +17,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
--- Player.gd
+++ Player.gd
@@ -23,9 +23,9 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 	if Input.is_action_pressed("move_up"):
-		velocity.y += 1
-	if Input.is_action_pressed("move_down"):
 		velocity.y -= 1
+	if Input.is_action_pressed("move_down"):
+		velocity.y += 1
 
 	if velocity.length() > 0:
 		velocity = velocity.normalized() * speed
@@ -54,20 +54,4 @@ func start(pos):
 	position = pos
 	show()
 	$CollisionShape2D.disabled = false
-	
-
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
