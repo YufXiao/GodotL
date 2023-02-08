ce7bd20f06137cfe54feb375004c8e5c9e1b4926
Yufeng Xiao <yufeng.xiao@uzh.ch>
2023-02-08 23:55:03 +0100

    updated

--- Main.gd
+++ Main.gd
@@ -48,6 +48,7 @@ func _on_MobTimer_timeout():
 
 func _on_ScoreTimer_timeout():
 	score += 1
+	$HUD.update_score(score)
 
 
 func _on_StartTimer_timeout():
@@ -58,11 +59,15 @@ func _on_StartTimer_timeout():
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
+	$HUD.show_game_over()
 
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready")
+	get_tree().call_group("mobs", "queue_free")
 
 
 
